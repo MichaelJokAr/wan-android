8a701553d968a07bbd11bf9a6912534d69abb004
guidongqi <guidongqi@hupu.com>
2019-12-09 18:44:56 +0800

    feat：使用Java8

---
+++ app/src/main/java/com/github/jokar/wan_android/app/WanAndroidApplication.kt
@@ -0,0 +1,11 @@
+package com.github.jokar.wan_android.app
+
+import android.app.Application
+
+/**
+ * application
+ * Create by JokAr at 2019.12.9
+ */
+class WanAndroidApplication :Application() {
+
+}
